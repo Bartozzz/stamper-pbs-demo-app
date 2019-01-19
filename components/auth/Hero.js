9d519a50f491a553484e0f7c9233c662e6804876
Bartosz Łaniewski <bartosz@laniewski.me>
2019-01-19 21:37:11 +0100

    Create Auth Hero component

---
+++ components/auth/Hero.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import colors from "../../constants/Colors";
+import layout from "../../constants/Layout";
+
+export default function AuthHero() {
+  return (
+    <View style={styles.hero}>
+      <Text style={styles.heroTextA}>Dołącz do</Text>
+      <Text style={styles.heroTextA}>bardziej lojalnych!</Text>
+      <Text style={styles.heroTextB}>Z nami wiele możesz, nic nie musisz.</Text>
+    </View>
+  );
+}
+
+export const styles = StyleSheet.create({
+  hero: {
+    height: 430,
+    backgroundColor: colors.background,
+
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  heroTextA: {
+    marginVertical: 2,
+    marginHorizontal: 0,
+
+    color: colors.color,
+
+    fontSize: 26,
+    fontWeight: "900",
+    fontFamily: layout.fontHead,
+    textAlign: "center",
+    textTransform: "uppercase"
+  },
+
+  heroTextB: {
+    marginTop: 20,
+
+    color: colors.border,
+
+    fontSize: 18,
+    fontFamily: layout.fontText,
+    textAlign: "center"
+  }
+});
