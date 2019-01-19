d1ff13cb7fa42556fa74ed2fd2a9e485b996013e
Bartosz Łaniewski <bartosz@laniewski.me>
2019-01-19 23:25:57 +0100

    Allow custom auth hero styles

--- components/auth/Hero.js
+++ components/auth/Hero.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 import colors from "../../constants/Colors";
 import layout from "../../constants/Layout";
 
-export default function AuthHero() {
+export default function AuthHero(props) {
   return (
-    <View style={styles.hero}>
+    <View style={[styles.hero, props.style]}>
       <Text style={styles.heroTextA}>Dołącz do</Text>
       <Text style={styles.heroTextA}>bardziej lojalnych!</Text>
       <Text style={styles.heroTextB}>Z nami wiele możesz, nic nie musisz.</Text>
@@ -16,7 +16,7 @@ export default function AuthHero() {
 
 export const styles = StyleSheet.create({
   hero: {
-    height: 430,
+    height: 350,
     backgroundColor: colors.background,
 
     justifyContent: "center",
