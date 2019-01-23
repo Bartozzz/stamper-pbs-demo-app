f32f3413ff271d19f980c8bb8d9ffa82fc310c39
Bartosz Łaniewski <bartosz@laniewski.me>
2019-01-23 18:27:14 +0100

    Accept array of errors in Error component

--- components/Error.js
+++ components/Error.js
@@ -4,13 +4,30 @@ import colors from "../constants/Colors";
 import layout from "../constants/Layout";
 
 class Error extends Component {
+  get message() {
+    const { message } = this.props;
+
+    if (Array.isArray(message)) {
+      return message.join(". ");
+    } else if (typeof message === "string") {
+      return message;
+    } else {
+      console.error(message);
+      return "Internal error";
+    }
+  }
+
   render() {
+    const message = this.message;
+
+    if (!message) {
+      return null;
+    }
+
     return (
       <View style={styles.errorContainer}>
         <Text style={styles.errorHead}>Something bad happened:</Text>
-        <Text style={styles.errorText}>
-          {this.props.message || "Internal error"}
-        </Text>
+        <Text style={styles.errorText}>{message}</Text>
       </View>
     );
   }
