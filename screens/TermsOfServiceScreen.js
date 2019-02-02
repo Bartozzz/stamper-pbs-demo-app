10c9aa792170fb56a2cf890631367c0a903f95b9
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-02 19:41:01 +0100

    Implement TermsOfService screen

--- screens/TermsOfServiceScreen.js
+++ screens/TermsOfServiceScreen.js
@@ -1,36 +1,76 @@
 import React from "react";
-import { StyleSheet, View, ScrollView, Dimensions } from "react-native";
+import { ActivityIndicator, View, Dimensions } from "react-native";
 import HTML from "react-native-render-html";
 
+import Background from "../components/Background";
+import Error from "../components/Error";
+
 import i18n from "../translations";
+import Url from "../constants/Urls";
+import defaultStyles from "../constants/Styles";
 import colors from "../constants/Colors";
+import axios from "../store/axios";
+
+const BackgroundImage = require("../assets/backgrounds/tos.png");
+
+// @see https://www.npmjs.com/package/react-native-render-html#styling
+const classesStyles = {};
+const tagsStyles = {
+  p: { color: "white" }
+};
 
 class TermsOfServiceScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: i18n.t("navigation.tos")
   });
 
+  state = {
+    loading: true,
+    content: "",
+    error: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get(Url.Account.GetTermsAndConditions(), {
+        responseType: "text"
+      })
+      .then(response => {
+        this.setState({
+          loading: false,
+          content: response.data
+        });
+      })
+      .catch(error => {
+        this.setState({
+          loading: false,
+          error: i18n.t("errors.tos.fetch")
+        });
+      });
+  }
+
   render() {
+    const { loading, content, error } = this.state;
+
     return (
-      <View style={styles.container}>
-        <ScrollView>
+      <Background source={BackgroundImage}>
+        {loading ? (
+          <View style={[defaultStyles.container, defaultStyles.center]}>
+            <ActivityIndicator color={colors.primary} size="large" />
+          </View>
+        ) : error ? (
+          <Error message={error} />
+        ) : (
           <HTML
-            html={`
-<h1>Regulamin</h1>
-          `}
+            html={content}
             imagesMaxWidth={Dimensions.get("window").width}
+            tagsStyles={tagsStyles}
+            classesStyles={classesStyles}
           />
-        </ScrollView>
-      </View>
+        )}
+      </Background>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background
-  }
-});
 
 export default TermsOfServiceScreen;
