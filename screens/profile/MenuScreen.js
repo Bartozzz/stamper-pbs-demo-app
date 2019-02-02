b94eb71e08ba73f2f0e5c2d79f4db80a533d0305
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-02 15:36:40 +0100

    Update Menu screen to match mockups

--- screens/profile/MenuScreen.js
+++ screens/profile/MenuScreen.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
 import Hamburger from "../../components/Hamburger";
 
 import i18n from "../../translations";
@@ -9,6 +16,9 @@ import defaultStyles from "../../constants/Styles";
 import colors from "../../constants/Colors";
 import layout from "../../constants/Layout";
 
+const BackgroundImage = require("../../assets/backgrounds/profile.png");
+const LogoImage = require("../../assets/logos/stamper.png");
+
 class ProfileMenuScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: i18n.t("navigation.profile.menu"),
@@ -16,7 +26,8 @@ class ProfileMenuScreen extends React.Component {
     headerRight: <Hamburger navigation={navigation} />,
     headerStyle: {
       borderBottomWidth: 0,
-      backgroundColor: colors.background
+      // backgroundColor: colors.background,
+      backgroundColor: "#001432"
     }
     // headerTransparent: true
   });
@@ -26,57 +37,67 @@ class ProfileMenuScreen extends React.Component {
 
     return (
       <View style={defaultStyles.container}>
-        <View style={styles.menu}>
-          <Image style={styles.avatar} source={{ uri: photo }} />
+        <ImageBackground
+          source={BackgroundImage}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <Image
+            source={LogoImage}
+            style={[styles.logo, { width: 195, height: 70 }]}
+          />
 
-          <Text style={styles.login}>{nickname}</Text>
-          <Text style={styles.email}>{email}</Text>
+          <View style={styles.menu}>
+            <Image style={styles.avatar} source={{ uri: photo }} />
 
-          <View style={styles.menuSpacer} />
+            <Text style={styles.login}>{nickname}</Text>
+            <Text style={styles.email}>{email}</Text>
 
-          <TouchableOpacity
-            style={styles.menuItem}
-            onPress={() => navigation.navigate(Routes.PROFILE_EDIT)}
-          >
-            <Text style={styles.menuItemText}>
-              {i18n.t("profile.menu.edit")}
-            </Text>
-          </TouchableOpacity>
+            <View style={styles.menuSpacer} />
 
-          <View style={styles.menuSpacer} />
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate(Routes.PROFILE_EDIT)}
+            >
+              <Text style={styles.menuItemText}>
+                {i18n.t("profile.menu.edit")}
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.menuItem}
-            onPress={() => navigation.navigate(Routes.PROFILE_PASSWORD)}
-          >
-            <Text style={styles.menuItemText}>
-              {i18n.t("profile.menu.password")}
-            </Text>
-          </TouchableOpacity>
+            <View style={styles.menuSpacer} />
 
-          <View style={styles.menuSpacer} />
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate(Routes.PROFILE_PASSWORD)}
+            >
+              <Text style={styles.menuItemText}>
+                {i18n.t("profile.menu.password")}
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.menuItem}
-            onPress={() => navigation.navigate(Routes.PROFILE_TOS)}
-          >
-            <Text style={styles.menuItemText}>
-              {i18n.t("profile.menu.tos")}
-            </Text>
-          </TouchableOpacity>
+            <View style={styles.menuSpacer} />
 
-          <View style={styles.menuSpacer} />
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate(Routes.PROFILE_TOS)}
+            >
+              <Text style={styles.menuItemText}>
+                {i18n.t("profile.menu.tos")}
+              </Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.menuItem}
-            style={styles.menuItem}
-            onPress={() => navigation.navigate(Routes.PROFILE_LOGOUT)}
-          >
-            <Text style={styles.menuItemText}>
-              {i18n.t("profile.menu.logout")}
-            </Text>
-          </TouchableOpacity>
-        </View>
+            <View style={styles.menuSpacer} />
+
+            <TouchableOpacity
+              style={styles.menuItem}
+              style={styles.menuItem}
+              onPress={() => navigation.navigate(Routes.PROFILE_LOGOUT)}
+            >
+              <Text style={styles.menuItemText}>
+                {i18n.t("profile.menu.logout")}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -84,36 +105,43 @@ class ProfileMenuScreen extends React.Component {
 
 const styles = StyleSheet.create({
   avatar: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 70,
 
-    borderRadius: 40,
+    borderRadius: 35,
     borderWidth: 4,
     borderColor: colors.border
   },
 
+  logo: {
+    alignSelf: "center"
+  },
+
   login: {
-    marginVertical: 15,
+    marginVertical: 17,
 
     color: colors.color,
-    fontSize: 18,
+    fontSize: 16,
     fontFamily: layout.fontHead
   },
 
   email: {
-    marginBottom: 30,
+    marginBottom: 35,
 
-    color: colors.label
+    color: colors.info,
+    fontSize: 10,
+    fontFamily: layout.fontText
   },
 
   menu: {
-    flex: 1,
     alignItems: "center",
 
+    paddingVertical: 35,
     marginVertical: 45,
     marginHorizontal: 40,
 
-    borderRadius: 10
+    borderRadius: 10,
+    backgroundColor: colors.background
   },
   menuSpacer: {
     width: 50,
