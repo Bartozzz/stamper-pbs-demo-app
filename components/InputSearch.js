b27b42cb89f9f445feaa4970dc96a9568812a8d4
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-03 00:10:53 +0100

    Implement InputSearch component

---
+++ components/InputSearch.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../constants/Colors";
+import layout from "../constants/Layout";
+
+class InputSearch extends Component {
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <Ionicons
+          style={styles.inputIcon}
+          name="ios-search"
+          size={16}
+          color={colors.info}
+        />
+
+        <TextInput
+          {...this.props}
+          placeholderTextColor={colors.info}
+          style={styles.inputStyle}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+
+    marginHorizontal: 20,
+    marginVertical: 0,
+
+    height: 40,
+    width: 265,
+
+    borderRadius: 10,
+    backgroundColor: "#1A5BF1"
+  },
+
+  inputIcon: {
+    paddingHorizontal: 10
+  },
+
+  inputStyle: {
+    flex: 1,
+    paddingRight: 10,
+    paddingLeft: 0,
+
+    fontSize: 14,
+    fontFamily: layout.fontText,
+    color: colors.info
+  }
+});
+
+export default InputSearch;
