4a926dd9ff12f71195a4b2c35d9bd02c22923baa
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-02 14:22:21 +0100

    Create getErrorsFromResponse util

---
+++ helpers/errors.js
@@ -0,0 +1,35 @@
+export function toArray(error) {
+  if (Array.isArray(error)) {
+    return error;
+  } else {
+    return [error];
+  }
+}
+
+export function getErrorsFromResponse(response, defaultErrors = {}) {
+  const errors = { ...defaultErrors };
+
+  if (typeof response === "string") {
+    if (Reflect.has(errors, "other")) {
+      // If error response is a string, put it to other:
+      errors.other = toArray(response);
+    }
+  } else if (typeof response !== "object" || response === null) {
+    // If error response is not an object, there's no response:
+    if (Reflect.has(errors, "other")) {
+      errors.other = toArray("unauthorized");
+    }
+  } else {
+    // Otherwise, iterate all errors:
+    Object.entries(response).forEach(error => {
+      const errorField = error[0].toLowerCase();
+      const errorValue = error[1];
+
+      if (Reflect.has(errors, errorField)) {
+        errors[errorField] = toArray(errorValue);
+      }
+    });
+  }
+
+  return errors;
+}
