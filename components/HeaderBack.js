84398cef9b104b3be65b6990538cf71c546e4ceb
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-14 19:45:03 +0100

    Implement HeaderBackButton component

---
+++ components/HeaderBack.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Platform, TouchableOpacity } from "react-native";
+
+const styles = StyleSheet.create({
+  backIcon: {
+    ...Platform.select({
+      ios: {
+        paddingLeft: 20
+      },
+      android: {
+        paddingRight: 8,
+        paddingLeft: 16
+      }
+    })
+  }
+});
+
+export default function BackButton(props) {
+  return (
+    <TouchableOpacity onPress={() => props.navigation.navigate("Dashboard")}>
+      <Ionicons
+        style={styles.backIcon}
+        name={Platform.select({
+          ios: "ios-arrow-round-back",
+          android: "md-arrow-round-back"
+        })}
+        size={Platform.select({
+          ios: 40,
+          android: 24
+        })}
+        color="white"
+      />
+    </TouchableOpacity>
+  );
+}
