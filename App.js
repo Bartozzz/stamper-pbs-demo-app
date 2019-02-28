b8354c60f1bd14621de04cfd73008f7ff95c56b0
Bartosz Łaniewski <bartosz@laniewski.me>
2019-02-28 17:01:49 +0100

    Add missing fonts

--- App.js
+++ App.js
@@ -126,9 +126,42 @@ export default class App extends React.Component {
       // Load fonts:
       Font.loadAsync({
         ...Icon.Ionicons.font,
+        // Space Mono:
         "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
-        "poppins-black": require("./assets/fonts/Poppins-Black.otf"),
-        "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf")
+        // Poppins:,
+        "poppins-thin": require("./assets/fonts/Poppins-Thin.ttf"),
+        "poppins-thin-italic": require("./assets/fonts/Poppins-ThinItalic.ttf"),
+        "poppins-light": require("./assets/fonts/Poppins-Light.ttf"),
+        "poppins-light-italic": require("./assets/fonts/Poppins-LightItalic.ttf"),
+        "poppins-extra-light": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+        "poppins-extra-light-italic": require("./assets/fonts/Poppins-ExtraLightItalic.ttf"),
+        "poppins-italic": require("./assets/fonts/Poppins-Italic.ttf"),
+        "poppins-regular": require("./assets/fonts/Poppins-Regular.ttf"),
+        "poppins-medium": require("./assets/fonts/Poppins-Medium.ttf"),
+        "poppins-medium-italic": require("./assets/fonts/Poppins-MediumItalic.ttf"),
+        "poppins-black": require("./assets/fonts/Poppins-Black.ttf"),
+        "poppins-black-italic": require("./assets/fonts/Poppins-BlackItalic.ttf"),
+        "poppins-semi-bold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+        "poppins-semi-bold-italic": require("./assets/fonts/Poppins-SemiBoldItalic.ttf"),
+        "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
+        "poppins-bold-italic": require("./assets/fonts/Poppins-BoldItalic.ttf"),
+        "poppins-extra-bold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+        "poppins-extra-bold-italic": require("./assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+        // Nunito:
+        "nunito-light": require("./assets/fonts/Nunito-Light.ttf"),
+        "nunito-light-italic": require("./assets/fonts/Nunito-LightItalic.ttf"),
+        "nunito-extra-light": require("./assets/fonts/Nunito-ExtraLight.ttf"),
+        "nunito-extra-light-italic": require("./assets/fonts/Nunito-ExtraLightItalic.ttf"),
+        "nunito-italic": require("./assets/fonts/Nunito-Italic.ttf"),
+        "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+        "nunito-black": require("./assets/fonts/Nunito-Black.ttf"),
+        "nunito-black-italic": require("./assets/fonts/Nunito-BlackItalic.ttf"),
+        "nunito-semi-bold": require("./assets/fonts/Nunito-SemiBold.ttf"),
+        "nunito-semi-bold-italic": require("./assets/fonts/Nunito-SemiBoldItalic.ttf"),
+        "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+        "nunito-bold-italic": require("./assets/fonts/Nunito-BoldItalic.ttf"),
+        "nunito-extra-bold": require("./assets/fonts/Nunito-ExtraBold.ttf"),
+        "nunito-extra-bold-italic": require("./assets/fonts/Nunito-ExtraBoldItalic.ttf")
       })
     ]);
   };
