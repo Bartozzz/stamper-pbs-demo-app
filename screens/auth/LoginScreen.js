4e088d57277fe0f286a8c911b26c20c2815b2bb0
Bartosz Łaniewski <bartosz@laniewski.me>
2019-04-03 00:14:34 +0200

    Use KeyboardAware and animations inside LoginScreen

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Facebook, Google } from "expo";
 import { connect } from "react-redux";
 import {
+  Animated,
   StyleSheet,
   Text,
   AsyncStorage,
   TouchableOpacity,
   ScrollView,
   View,
-  Keyboard,
-  KeyboardAvoidingView,
   Dimensions
 } from "react-native";
 import Background from "../../components/Background";
 
+import KeyboardAware from "../../components/helpers/KeyboardAware";
 import AuthHero from "../../components/auth/Hero";
 import Button from "../../components/Button";
 import Error from "../../components/Error";
@@ -35,13 +35,16 @@ import { getErrorsFromResponse } from "../../helpers/errors";
 
 const BackgroundImage = require("../../assets/backgrounds/password_wn.png");
 
+const heroMinWidth = 280;
+const heroMaxWidth = Math.max(340, (Dimensions.get("window").height - 170) / 2);
+
 class AuthLoginScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
 
   state = {
-    isKeyboardVisible: false,
+    topAnim: new Animated.Value(0),
 
     password: __DEV__ ? "Test1234+" : null,
     email: __DEV__ ? "testing@test.pl" : null,
@@ -51,35 +54,6 @@ class AuthLoginScreen extends React.Component {
       email: null,
       other: null
     }
-  };
-
-  componentDidMount() {
-    this.keyboardDidShowListener = Keyboard.addListener(
-      "keyboardWillShow",
-      this.handleKeyboardShow
-    );
-
-    this.keyboardDidHideListener = Keyboard.addListener(
-      "keyboardWillHide",
-      this.handleKeyboardHide
-    );
-  }
-
-  componentWillUnmount() {
-    this.keyboardDidShowListener.remove();
-    this.keyboardDidHideListener.remove();
-  }
-
-  handleKeyboardShow = () => {
-    this.setState({
-      isKeyboardVisible: true
-    });
-  };
-
-  handleKeyboardHide = () => {
-    this.setState({
-      isKeyboardVisible: false
-    });
   };
 
   loginExternal = email => {
@@ -193,56 +167,77 @@ class AuthLoginScreen extends React.Component {
     this.props.navigation.navigate(Routes.AUTH_RESET);
   };
 
+  handleKeyboardShow = keyboardHeight => {
+    Animated.timing(this.state.topAnim, {
+      toValue: -keyboardHeight,
+      duration: 250
+    }).start();
+  };
+
+  handleKeyboardHide = () => {
+    Animated.timing(this.state.topAnim, {
+      toValue: 0,
+      duration: 250
+    }).start();
+  };
+
   render() {
-    const { isKeyboardVisible, email, password, error } = this.state;
+    const { topAnim, email, password, error } = this.state;
 
     return (
-      <KeyboardAvoidingView style={defaultStyles.grow} behavior="padding">
-        <Background source={BackgroundImage} disableScroll>
-          <AuthHero
-            style={[styles.hero, isKeyboardVisible && { display: "none" }]}
-          />
+      <KeyboardAware
+        onKeyboardShow={this.handleKeyboardShow}
+        onKeyboardHide={this.handleKeyboardHide}
+      >
+        {() => (
+          <View style={defaultStyles.container}>
+            <AuthHero style={[styles.hero]} />
 
-          <ScrollView style={styles.loginContainer}>
-            {error.other ? (
-              <Error message={i18n.t("errors.auth.unauthorized")} />
-            ) : null}
+            <Animated.View style={[{ height: 350 }, { top: topAnim }]}>
+              <Background source={BackgroundImage} disableScroll>
+                <ScrollView style={styles.loginContainer}>
+                  {error.other ? (
+                    <Error message={i18n.t("errors.auth.unauthorized")} />
+                  ) : null}
 
-            <InputWithIcon
-              iconName="ios-contact"
-              iconSize={20}
-              placeholder={i18n.t("auth.email")}
-              value={email}
-              error={error.email}
-              onChangeText={email => this.setState({ email })}
-              autoCapitalize="none"
-            />
+                  <InputWithIcon
+                    iconName="ios-contact"
+                    iconSize={20}
+                    placeholder={i18n.t("auth.email")}
+                    value={email}
+                    error={error.email}
+                    onChangeText={email => this.setState({ email })}
+                    autoCapitalize="none"
+                  />
 
-            <InputWithIcon
-              iconName="ios-lock"
-              iconSize={20}
-              placeholder={i18n.t("auth.password")}
-              value={password}
-              error={error.password}
-              onChangeText={password => this.setState({ password })}
-              autoCapitalize="none"
-              secureTextEntry
-            />
+                  <InputWithIcon
+                    iconName="ios-lock"
+                    iconSize={20}
+                    placeholder={i18n.t("auth.password")}
+                    value={password}
+                    error={error.password}
+                    onChangeText={password => this.setState({ password })}
+                    autoCapitalize="none"
+                    secureTextEntry
+                  />
 
-            <AuthLoginScreenLinks
-              loginWithFacebook={this.loginWithFacebook}
-              loginWithGoogle={this.loginWithGoogle}
-              navigateToRegister={this.navigateToRegister}
-              navigateToResetPassword={this.navigateToResetPassword}
-            />
+                  <AuthLoginScreenLinks
+                    loginWithFacebook={this.loginWithFacebook}
+                    loginWithGoogle={this.loginWithGoogle}
+                    navigateToRegister={this.navigateToRegister}
+                    navigateToResetPassword={this.navigateToResetPassword}
+                  />
 
-            <Button
-              title={i18n.t("auth.login")}
-              onPress={this.loginWithCredentials}
-            />
-          </ScrollView>
-        </Background>
-      </KeyboardAvoidingView>
+                  <Button
+                    title={i18n.t("auth.login")}
+                    onPress={this.loginWithCredentials}
+                  />
+                </ScrollView>
+              </Background>
+            </Animated.View>
+          </View>
+        )}
+      </KeyboardAware>
     );
   }
 }
@@ -305,8 +300,8 @@ export const AuthLoginScreenLinks = props => (
 const styles = StyleSheet.create({
   hero: {
     flex: 1,
-    minHeight: 280,
-    maxHeight: Math.max(340, (Dimensions.get("window").height - 170) / 2)
+    minHeight: heroMinWidth,
+    maxHeight: heroMaxWidth
   },
 
   loginProvider: {
