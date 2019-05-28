649d2e24b5b45f0f63e5a172210c521bfbf49919
Bartosz Łaniewski <bartosz@laniewski.me>
2019-05-28 22:21:45 +0200

    fix(MAS-76): Fix first log-in with external provider

---
+++ helpers/auth.js
@@ -0,0 +1,54 @@
+import { Facebook, Google, Constants } from "expo";
+import { Platform } from "react-native";
+
+let FACEBOOK_APP_ID;
+let GOOGLE_CLIENT_ID;
+
+if (Platform.OS === "ios") {
+  FACEBOOK_APP_ID = Constants.manifest.extra.ios.FACEBOOK_APP_ID;
+  GOOGLE_CLIENT_ID = Constants.manifest.extra.ios.GOOGLE_CLIENT_ID;
+} else {
+  FACEBOOK_APP_ID = Constants.manifest.extra.android.FACEBOOK_APP_ID;
+  GOOGLE_CLIENT_ID = Constants.manifest.extra.android.GOOGLE_CLIENT_ID;
+}
+
+export async function fetchUserByFacebookAccessToken(token) {
+  const endpoint = "https://graph.facebook.com/me?fields=email";
+
+  // Get the user's email using Facebook's Graph API:
+  const resp = await fetch(`${endpoint}&access_token=${token}`);
+  const data = await resp.json();
+
+  return data;
+}
+
+export async function logInWithGoogle(onSuccess, onError) {
+  try {
+    const clientId = GOOGLE_CLIENT_ID;
+    const { type, user } = await Google.logInAsync({ clientId });
+
+    if (type !== "success") {
+      onError("Could not log-in with Google");
+    } else {
+      onSuccess(user);
+    }
+  } catch (error) {
+    onError(error);
+  }
+}
+
+export async function logInWithFacebook(onSuccess, onError) {
+  try {
+    const fid = FACEBOOK_APP_ID;
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(fid);
+
+    if (type !== "success") {
+      onError("Could not log-in with Facebook");
+    } else {
+      const user = await fetchUserByFacebookAccessToken(token);
+      onSuccess(user);
+    }
+  } catch (error) {
+    onError(error);
+  }
+}
