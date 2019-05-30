afb9a46f666045a419cdd77b99f9742f322697cf
Bartosz Łaniewski <bartosz@laniewski.me>
2019-05-30 12:25:26 +0200

    fix(MAS-87): Update HTML styles for external terms of service

--- components/screens/terms/TermsAsHTML.js
+++ components/screens/terms/TermsAsHTML.js
@@ -1,5 +1,12 @@
+/* eslint-disable react/display-name */
 import React from "react";
-import { StyleSheet, ActivityIndicator, View, Linking } from "react-native";
+import {
+  StyleSheet,
+  ActivityIndicator,
+  View,
+  Text,
+  Linking
+} from "react-native";
 import HTML from "react-native-render-html";
 
 import Error from "../../Error";
@@ -26,23 +33,71 @@ function TermsAsHTML({ loading, content, error }) {
 
   return (
     <View style={styles.container}>
-      <HTML html={content} tagsStyles={tagsStyles} onLinkPress={openLink} />
+      <HTML
+        html={content}
+        tagsStyles={tagsStyles}
+        onLinkPress={openLink}
+        listsPrefixesRenderers={{
+          ul: () => (
+            <View
+              style={{
+                marginRight: 10,
+                width: 12 / 2.8,
+                height: 12 / 2.8,
+                marginTop: 12 / 2,
+                borderRadius: 12 / 2.8,
+                backgroundColor: "white"
+              }}
+            />
+          ),
+          ol: (_htmlAttribs, _children, _convertedCSSStyles, passProps) => (
+            <Text style={{ marginRight: 5, fontSize: 12, color: "white" }}>
+              {passProps.index + 1})
+            </Text>
+          )
+        }}
+      />
     </View>
   );
 }
 
+const headingStyle = {
+  color: "white"
+};
+
+const textStyle = {
+  color: "#95989A",
+  fontSize: 12
+};
+
+const listStyle = {
+  marginTop: 5
+};
+
 // @see https://www.npmjs.com/package/react-native-render-html#styling
 const tagsStyles = {
+  h1: {
+    ...headingStyle,
+    marginBottom: 10
+  },
   h2: {
-    color: "white",
+    ...headingStyle,
     fontSize: 16,
 
     marginTop: 20,
     marginBottom: 4
   },
+  ol: {
+    ...listStyle
+  },
+  ul: {
+    ...listStyle
+  },
   p: {
-    color: "#95989A",
-    fontSize: 12
+    ...textStyle
+  },
+  li: {
+    ...textStyle
   }
 };
 
