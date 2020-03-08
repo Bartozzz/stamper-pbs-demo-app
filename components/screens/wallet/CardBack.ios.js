99fab4456207fb6a4cb4d4e7e8f614f0a0cf1685
Maciej Synowski <maciejsynowski@protonmail.com>
2020-03-08 11:24:24 +0100

    Wallet components refactor

--- components/screens/wallet/CardBack.ios.js
+++ components/screens/wallet/CardBack.ios.js
@@ -1,49 +1,44 @@
 import * as React from "react";
-import { StyleSheet, Image, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
 const DeleteImage = require("../../../assets/images/delete.png");
 
 const height = 90;
 
+const ItemRemove = styled.TouchableOpacity`
+  align.items: center;
+  justify-content: center;
+
+  position: absolute;
+  top: 10px;
+  right: ${-height}px;
+
+  height: ${height}px;
+  width: ${height}px;
+
+  /* background-color: #555f6f; */
+  background-color: #f16c41;
+  border-radius: 10px;
+
+  /* Need to add zIndex to ensure that the TouchableOpacity will receive press */
+  /* events on Android: */
+  zIndex: 1;
+`;
+
+const RemoveImage = styled.Image.attrs(props => ({
+  source: DeleteImage
+}))`
+  width: 40px;
+  height: 40px;
+`;
+
 export const CardBack = ({ data, onPress }) => {
   return (
-    <TouchableOpacity style={styles.itemRemove} onPress={onPress}>
-      <Image source={DeleteImage} style={styles.itemRemoveImage} />
-    </TouchableOpacity>
+    <ItemRemove onPress={onPress}>
+      <RemoveImage/>
+    </ItemRemove>
   );
 };
 
-const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    marginHorizontal: 15,
-    marginVertical: 10,
-
-    borderRadius: 10
-  },
-  itemRemove: {
-    alignItems: "center",
-    justifyContent: "center",
-
-    position: "absolute",
-    top: 10,
-    right: -height,
-
-    height: height,
-    width: height,
-
-    // backgroundColor: "#555f6f",
-    backgroundColor: "#f16c41",
-    borderRadius: 10,
-
-    // Need to add zIndex to ensure that the TouchableOpacity will receive press
-    // events on Android:
-    zIndex: 1
-  },
-  itemRemoveImage: {
-    width: 40,
-    height: 40
-  }
-});
 
 export default CardBack;
