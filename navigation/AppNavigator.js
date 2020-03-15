95a3ddab90efcf722df4f5a12927a2cb634edad0
Maciej Synowski <maciejsynowski@protonmail.com>
2020-03-15 23:57:57 +0100

    AppNavigator rewrite to components

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,17 @@
-import {
-  createStackNavigator,
-  createSwitchNavigator,
-  createAppContainer
-} from "react-navigation";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import * as Route from "./index";
 import colors from "../constants/Colors";
 import layout from "../constants/Layout";
+import defaultStyles from "../constants/Styles";
+
+import HeaderTitle from "../components/nav/HeaderTitle";
+import HeaderEmpty from "../components/nav/HeaderEmpty";
+import HeaderBackIcon from "../components/nav/HeaderBack";
+
+import i18n from "../translations";
 
 import InfoErrorScreen from "../screens/info/ErrorScreen";
 import InfoSuccessScreen from "../screens/info/SuccessScreen";
@@ -39,10 +44,13 @@ import WalletPlacesScreen from "../screens/wallet/PlacesScreen";
 import WalletCardRemovalConfirmationScreen from "../screens/wallet/CardRemovalConfirmationScreen";
 import TermsOfServiceScreen from "../screens/TermsOfServiceScreen";
 
-const defaultNavigationOptions = {
-  headerTintColor: colors.color,
-  // headerBackTitle: null,
 
+const Stack = createStackNavigator();
+const defaultNavigationOptions = {
+  animationEnabled: false,
+  gestureEnabled: true,
+  headerTitleAlign: "left",
+  headerTintColor: "red",
   headerStyle: {
     marginTop: layout.headerTopSpacer,
 
@@ -50,7 +58,6 @@ const defaultNavigationOptions = {
     borderBottomWidth: 0,
     backgroundColor: colors.primary
   },
-
   headerTitleStyle: {
     flex: 1,
 
@@ -59,102 +66,276 @@ const defaultNavigationOptions = {
 
     alignSelf: "center",
     textAlign: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   }
-};
+ };
 
-const disableNavigationAnimations = {
-  transitionConfig: () => ({
-    transitionSpec: {
-      // Disable animations:
-      duration: -1
-    }
-  })
-};
-
-const AuthStack = createStackNavigator(
-  {
-    [Route.AUTH_LOGIN]: AuthLoginScreen,
-    [Route.AUTH_REGISTER]: AuthRegisterScreen,
-    [Route.AUTH_RESET]: AuthResetPasswordScreen,
-    [Route.AUTH_TOS]: TermsOfServiceScreen,
-    [Route.AUTH_EXTERNAL_TOS]: AuthTermsOfUseScreen
-  },
-  {
-    initialRouteName: Route.AUTH_LOGIN,
-    headerLayoutPreset: "left",
-    defaultNavigationOptions,
-    headerMode: "screen",
-    ...disableNavigationAnimations
-  }
-);
-
-const AppStack = createStackNavigator(
-  {
-    [Route.DASHBOARD]: DashboardMainScreen,
-    [Route.DASHBOARD_MAIN]: DashboardMainScreen,
-    [Route.DASHBOARD_TOS]: TermsOfServiceScreen,
-
-    [Route.PROFILE]: ProfileMenuScreen,
-    [Route.PROFILE_MENU]: ProfileMenuScreen,
-    [Route.PROFILE_EDIT]: ProfileEditScreen,
-    [Route.PROFILE_PASSWORD]: ProfilePasswordScreen,
-    [Route.PROFILE_LOGOUT]: ProfileLogoutScreen,
-    [Route.PROFILE_TOS]: TermsOfServiceScreen,
-    [Route.PROFILE_LEGAL]: ProfileLegalScreen,
-    [Route.PROFILE_NEWSLETTER_TOS]: ProfileNewsletterTermsScreen,
-    [Route.PROFILE_NEWSLETTER_UPDATE]: ProfileNewsletterUpdateScreen,
-
-    [Route.MAP]: MapScreen,
-    [Route.MAP_ALL]: MapScreen,
-    [Route.MAP_ACCEPT_CARD_TERMS]: MapAcceptCardTermsScreen,
-    [Route.MAP_CONFIRM_REFUSED_TERMS]: MapConfirmRefusedTermsScreen,
-
-    [Route.PRIZES]: PrizesAvailableScreen,
-    [Route.PRIZES_LIST]: PrizesAvailableScreen,
-    [Route.PRIZES_RECEIVED]: PrizesReceivedScreen,
-    [Route.PRIZES_SELECTED]: PrizesRewardCodeScreen,
-
-    [Route.WALLET]: WalletCardsScreen,
-    [Route.WALLET_CARDS]: WalletCardsScreen,
-    [Route.WALLET_PLACES]: WalletPlacesScreen,
-    [Route.WALLET_CARD_REMOVAL_CONFIRMATION]: WalletCardRemovalConfirmationScreen,
-
-    [Route.CARD]: CardInfoScreen,
-    [Route.CARD_INFO]: CardInfoScreen,
-
-    // The initial route is always mounted. If we set it to INFO_ERROR, it will
-    // trigger INFO_ERROR's setTimeout even if we navigte to INFO_SUCCESS:
-    [Route.INFO]: () => null,
-    [Route.INFO_INIT]: () => null,
-    [Route.INFO_ERROR]: InfoErrorScreen,
-    [Route.INFO_SUCCESS]: InfoSuccessScreen,
-
-    [Route.SCANNER]: ScannerScanScreen,
-    [Route.SCANNER_SCAN]: ScannerScanScreen,
-    [Route.SCANNER_ACCEPT_STAMP_TERMS]: ScannerAcceptStampTermsScreen,
-    [Route.SCANNER_CONFIRM_REFUSED_TERMS]: ScannerConfirmRefusedTermsScreen
-  },
-  {
-    initialRouteName: Route.DASHBOARD_MAIN,
-    headerLayoutPreset: "left",
-    defaultNavigationOptions,
-    headerMode: "screen",
-    ...disableNavigationAnimations
-  }
-);
-
-export default createAppContainer(
-  createSwitchNavigator(
-    {
-      [Route.AUTH_CONNECTIVITY_CHECK]: AuthConnectivityCheckScreen,
-      [Route.AUTH_LOADING]: AuthLoadingScreen,
-      [Route.AUTH]: AuthStack,
-      [Route.APP]: AppStack
-    },
-    {
-      initialRouteName: Route.AUTH_CONNECTIVITY_CHECK,
-      headerMode: "screen"
-    }
+function AuthStack() {
+  return(
+    <Stack.Navigator 
+    initialRouteName={Route.AUTH_LOGIN} 
+    headerMode="screen"
+  >
+      <Stack.Screen
+        options={{ 
+          title: i18n.t("navigation.auth.login"),
+          headerTitleAlign: "left",
+          headerTitle: HeaderTitle,
+          headerLeft: HeaderEmpty,
+          headerStyle: defaultStyles.headerTwoLines,
+        }}
+        name={Route.AUTH_LOGIN}
+        component={AuthLoginScreen}
+      />
+      <Stack.Screen
+        options={{
+          defaultNavigationOptions,
+          title: i18n.t("navigation.auth.register"),
+        }}
+        name={Route.AUTH_REGISTER}
+        component={AuthRegisterScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.AUTH_RESET}
+        component={AuthResetPasswordScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.AUTH_TOS}
+        component={TermsOfServiceScreen}
+      /> 
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.AUTH_EXTERNAL_TOS}
+        component={AuthTermsOfUseScreen}
+      /> 
+  </Stack.Navigator>
   )
-);
+};
+
+function AppStack() {
+  return(
+    <Stack.Navigator 
+      initialRouteName={Route.DASHBOARD_MAIN} 
+      headerMode="screen"
+    >
+      <Stack.Screen
+        options={{ 
+          defaultNavigationOptions,     
+          title: i18n.t("navigation.dashboard.main"),
+          headerShown: false 
+        }}
+        name={Route.DASHBOARD}
+        component={DashboardMainScreen}
+      />
+      <Stack.Screen
+        options={{ 
+          defaultNavigationOptions, 
+          title: i18n.t("navigation.dashboard.main"),
+          headerShown: false 
+        }}
+        name={Route.DASHBOARD_MAIN}
+        component={DashboardMainScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.DASHBOARD_TOS}
+        component={TermsOfServiceScreen}
+      />
+
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE}
+        component={ProfileMenuScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_MENU}
+        component={ProfileMenuScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_EDIT}
+        component={ProfileEditScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_PASSWORD}
+        component={ProfilePasswordScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_LOGOUT}
+        component={ProfileLogoutScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_TOS}
+        component={TermsOfServiceScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_LEGAL}
+        component={ProfileLegalScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_NEWSLETTER_TOS}
+        component={ProfileNewsletterTermsScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PROFILE_NEWSLETTER_UPDATE}
+        component={ProfileNewsletterUpdateScreen}
+      />
+      
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.MAP}
+        component={MapScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.MAP_ALL}
+        component={MapScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.MAP_ACCEPT_CARD_TERMS}
+        component={MapAcceptCardTermsScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.MAP_CONFIRM_REFUSED_TERMS}
+        component={MapConfirmRefusedTermsScreen}
+      />
+
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PRIZES}
+        component={PrizesAvailableScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PRIZES_LIST}
+        component={PrizesAvailableScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PRIZES_RECEIVED}
+        component={PrizesReceivedScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.PRIZES_SELECTED}
+        component={PrizesRewardCodeScreen}
+      />
+      
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.WALLET}
+        component={WalletCardsScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.WALLET_CARDS}
+        component={WalletCardsScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.WALLET_PLACES}
+        component={WalletPlacesScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.WALLET_CARD_REMOVAL_CONFIRMATION}
+        component={WalletCardRemovalConfirmationScreen}
+      />
+
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.CARD}
+        component={CardInfoScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.CARD_INFO}
+        component={CardInfoScreen}
+      />
+
+      {/* The initial route is always mounted. If we set it to INFO_ERROR, it will
+      trigger INFO_ERROR's setTimeout even if we navigte to INFO_SUCCESS: */}
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.INFO}
+        component={() => null}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.INFO_INIT}
+        component={() => null}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.INFO_ERROR}
+        component={InfoErrorScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.INFO_SUCCESS}
+        component={InfoSuccessScreen}
+      />
+      
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.SCANNER}
+        component={ScannerScanScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.SCANNER_SCAN}
+        component={ScannerScanScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.SCANNER_ACCEPT_STAMP_TERMS}
+        component={ScannerAcceptStampTermsScreen}
+      />
+      <Stack.Screen
+        options={ defaultNavigationOptions }
+        name={Route.SCANNER_CONFIRM_REFUSED_TERMS}
+        component={ScannerConfirmRefusedTermsScreen}
+      />
+  </Stack.Navigator>
+  )
+};
+
+export default function AppContainer() {
+ return (
+  <NavigationContainer initialRouteName={Route.AUTH_CONNECTIVITY_CHECK}>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen
+          options={ defaultNavigationOptions }
+          name={Route.AUTH_CONNECTIVITY_CHECK}
+          component={AuthConnectivityCheckScreen}
+        />
+        <Stack.Screen
+          options={ defaultNavigationOptions }
+          name={Route.AUTH_LOADING}
+          component={AuthLoadingScreen}
+        />
+        <Stack.Screen
+          options={ defaultNavigationOptions }
+          name={Route.AUTH}
+          component={AuthStack}
+        />
+        <Stack.Screen
+          options={ defaultNavigationOptions }
+          name={Route.APP}
+          component={AppStack}
+        />
+      </Stack.Navigator>
+  </NavigationContainer>
+ )
+}
