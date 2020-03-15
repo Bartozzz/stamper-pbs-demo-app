25944de6247981f64b3042a96944ab944a6e7871
Maciej Synowski <maciejsynowski@protonmail.com>
2020-03-15 23:58:16 +0100

    Add function to be compatible with React Navigation

--- components/nav/HeaderEmpty.js
+++ components/nav/HeaderEmpty.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const HeaderEmpty = styled.View`
+const Empty = styled.View`
   align-self: flex-start;
 
   width: 45px;
-  height: 40px;
+  height: 40px; 
 `;
+// React navigation throws errors if there's no function
+function HeaderEmpty() {
+  return <Empty></Empty>;
+}
 
 export default HeaderEmpty;
