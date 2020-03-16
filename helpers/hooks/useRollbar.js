847282451a3b988ab1256721cfd93533145bc1ed
Bartozzz <bartosz.laniewski@netguru.com>
2020-03-16 21:04:23 +0100

    [MAS-155] Add Rollbar integration

---
+++ helpers/hooks/useRollbar.js
@@ -0,0 +1,18 @@
+import { Platform } from "react-native";
+import Rollbar from "rollbar/src/react-native/rollbar";
+import { getEnv } from "../env";
+
+const rollbar = new Rollbar({
+  accessToken: "a0e890289a3442e0870d27aa59240c2d",
+
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  captureDeviceInfo: true,
+
+  environment: getEnv(),
+  platform: Platform.OS
+});
+
+export default function useRollbar() {
+  return rollbar;
+}
