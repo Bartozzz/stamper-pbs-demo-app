df8cf74eaa04f63ed3dac6eaebce76fea7ff3945
Maciej Synowski <maciejsynowski@protonmail.com>
2020-03-19 21:20:29 +0100

    Revert "Add function to be compatible with React Navigation"
    This reverts commit 25944de6247981f64b3042a96944ab944a6e7871.

--- components/nav/HeaderEmpty.js
+++ components/nav/HeaderEmpty.js
@@ -1,15 +1,11 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const Empty = styled.View`
+const HeaderEmpty = styled.View`
   align-self: flex-start;
 
   width: 45px;
-  height: 40px; 
+  height: 40px;
 `;
-// React navigation throws errors if there's no function
-function HeaderEmpty() {
-  return <Empty></Empty>;
-}
 
 export default HeaderEmpty;
