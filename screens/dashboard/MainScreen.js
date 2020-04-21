abb11aa4e656632b4d14d8d14738745b51539979
Bartozzz <bartosz.laniewski@netguru.com>
2020-04-21 21:01:56 +0200

    [MAS-164] Remove navigateTo method from MainScreen.js

--- screens/dashboard/MainScreen.js
+++ screens/dashboard/MainScreen.js
@@ -19,13 +19,6 @@ const MenuImageWallet = require("../../assets/images/menu/wallet.png");
 const MenuImageScanner = require("../../assets/images/menu/scanner.png");
 const BackgroundImage = require("../../assets/backgrounds/home_wn.png");
 
-const MENU_MAP = "MENU_MAP";
-const MENU_MARKET = "MENU_MARKET";
-const MENU_PRIZES = "MENU_PRIZES";
-const MENU_PROFILE = "MENU_PROFILE";
-const MENU_WALLET = "MENU_WALLET";
-const MENU_SCANNER = "MENU_SCANNER";
-
 class DashboardMainScreen extends React.Component {
   static navigationOptions = {
     title: i18n.t("navigation.dashboard.main"),
@@ -36,27 +29,8 @@ class DashboardMainScreen extends React.Component {
     this.props.getPrizesCount();
   }
 
-  navigateTo = (element) => () => {
-    const { navigation } = this.props;
-
-    switch (element) {
-      case MENU_MAP:
-        return navigation.push(Routes.MAP);
-      case MENU_WALLET:
-        return navigation.push(Routes.WALLET);
-      case MENU_PRIZES:
-        return navigation.push(Routes.PRIZES);
-      case MENU_PROFILE:
-        return navigation.push(Routes.PROFILE);
-      case MENU_MARKET:
-        return;
-      case MENU_SCANNER:
-        return navigation.push(Routes.SCANNER);
-    }
-  };
-
   render() {
-    const { prizesCount } = this.props;
+    const { navigation, prizesCount } = this.props;
     const { height } = Dimensions.get("window");
     const smallPhone = height <= 640 ? true : false;
 
@@ -68,14 +42,14 @@ class DashboardMainScreen extends React.Component {
           <View style={[defaultStyles.row, styles.row]}>
             <DashboardButton
               icon={MenuImageMap}
-              onPress={this.navigateTo(MENU_MAP)}
+              onPress={() => navigation.push(Routes.MAP)}
             >
               {i18n.t("dashboard.map")}
             </DashboardButton>
 
             <DashboardButton
               icon={MenuImageWallet}
-              onPress={this.navigateTo(MENU_WALLET)}
+              onPress={() => navigation.push(Routes.WALLET)}
             >
               {i18n.t("dashboard.wallet")}
             </DashboardButton>
@@ -85,30 +59,27 @@ class DashboardMainScreen extends React.Component {
             <DashboardButton
               icon={MenuImagePrizes}
               badge={prizesCount}
-              onPress={this.navigateTo(MENU_PRIZES)}
+              onPress={() => navigation.push(Routes.PRIZES)}
             >
               {i18n.t("dashboard.prizes")}
             </DashboardButton>
 
             <DashboardButton
               icon={MenuImageProfile}
-              onPress={this.navigateTo(MENU_PROFILE)}
+              onPress={() => navigation.push(Routes.PROFILE)}
             >
               {i18n.t("dashboard.profile")}
             </DashboardButton>
           </View>
 
           <View style={[defaultStyles.row, styles.row]}>
-            <DashboardButton
-              icon={MenuImageMarket}
-              onPress={this.navigateTo(MENU_MARKET)}
-            >
+            <DashboardButton icon={MenuImageMarket} onPress={() => {}}>
               {i18n.t("dashboard.market")}
             </DashboardButton>
 
             <DashboardButton
               icon={MenuImageScanner}
-              onPress={this.navigateTo(MENU_SCANNER)}
+              onPress={() => navigation.push(Routes.SCANNER)}
             >
               {i18n.t("dashboard.scanner")}
             </DashboardButton>
