755c66ca63415c3648b13046e9d1df2606e6cf75
Maciej Synowski <maciejsynowski@icloud.com>
2020-05-17 22:16:23 +0200

    [MAS-165] Cover CardBackground with tests

---
+++ components/CardBackground/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import CardBackground from './';
+
+const getByTestID = (component, ID) => {
+    return(component.findWhere((node) => node.prop('testID') === `${ID}` ))
+};
+
+describe("Card Background", () => {
+    it("Should render image if there's src", () => {
+        const component = mount(<CardBackground src="https://placekitten.com/200/300" />);
+        const image = getByTestID(component, "card-background");
+
+        expect(image).toBeTruthy();
+    });
+});
