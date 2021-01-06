b1ad89ef4aea0aa4365c30cdab45c5835de203f3
Maciej Synowski <34197298+Shizerq@users.noreply.github.com>
2021-01-06 20:27:43 +0100

    [MAS-263] Fix typos

--- components/layout/Box/index.theme.js
+++ components/layout/Box/index.theme.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
-import theme from "../../constants/theme";
+import theme from "../../../constants/theme";
 
 export const Theme = ({ children }) => {
   return (
     <ThemeProvider
       theme={{
-        backgroundColor: theme.colors.backgroundColor,
+        backgroundColor: theme.colors.background,
         headingColor: theme.colors.white,
         subheadingColor: theme.colors.white,
         actionColor: theme.colors.info,
